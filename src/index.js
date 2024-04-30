29cd2dfb62f1598871ff7a05ae7ad95dfeb53f83
smllns <marysmoly@gmail.com>
2024-04-30 17:39:31 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -23,13 +23,24 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <ChakraProvider>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </ChakraProvider>
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
